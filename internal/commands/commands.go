eb0d607329b8e74868fcedd7cea27d7b80adecf6
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-01-05 12:27:49 +1100

    moving cmd init

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -18,9 +18,11 @@ func init() {
 	rootCmd.PersistentFlags().BoolVarP(&flagRootVerbose, "verbose", "v", false, "add verbose logging")
 	rootCmd.PersistentFlags().BoolVarP(&flagRootDryRun, "dry-run", "d", false, "dry run mode outputs items to sdt out")
 	rootCmd.PersistentFlags().StringVarP(&flagRootAwsRegion, "aws-region", "r", "ap-southeast-2", "aws region, default is ap-southeast-2")
+
+	rootCmd.AddCommand(purgeCmd)
 }
 
 func Execute() error {
-	rootCmd.AddCommand(purgeCmd)
+
 	return rootCmd.Execute()
 }
