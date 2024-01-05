59470855fa0ab2408e04943a6a8dbcca4e670520
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-01-05 11:18:08 +1100

    adding command

--- go.mod
+++ go.mod
@@ -23,6 +23,9 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.26.6 // indirect
 	github.com/aws/smithy-go v1.19.0 // indirect
 	github.com/code-gorilla-au/env v0.0.0-20231101054621-7a54afca2a47 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 )
---
+++ internal/commands/commands.go
@@ -0,0 +1,25 @@
+package commands
+
+import "github.com/spf13/cobra"
+
+var (
+	flagRootVerbose   = false
+	flagRootDryRun    = false
+	flagRootAwsRegion = "ap-southeast-2"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "goety [COMMAND] --[FLAGS]",
+	Short: "dynamodb purge tool",
+	Long:  "dynamodb purge tool",
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&flagRootVerbose, "verbose", "v", false, "add verbose logging")
+	rootCmd.PersistentFlags().BoolVarP(&flagRootDryRun, "dry-run", "d", false, "dry run mode outputs items to sdt out")
+	rootCmd.PersistentFlags().StringVarP(&flagRootAwsRegion, "aws-region", "r", "ap-southeast-2", "aws region, default is ap-southeast-2")
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
